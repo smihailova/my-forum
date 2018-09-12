4b9fe90b2e57483c6b63a4921d159da973109110
smihailova <smihailova@thumbtack.net>
2018-09-12 21:20:42 +0600

    2 Added env config with db url, added manage file, added User, Post and Comment models, update requirements list, update gitignore

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from models import db
+from config import DevelopmentConfig
 
 app = Flask(__name__, static_folder='../build', template_folder='../src/assets')
-
+app.config.from_object(DevelopmentConfig)
+db.init_app(app)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
